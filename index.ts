70d4c2e20cd81ed9474f1a87a152256c329f0eab
Erik Hughes <erik.hughes@outlook.com>
2022-04-08 15:14:55 -0400

    feat: initial reproduction

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {
+  fakeModule1,
+  fakeModule2,
+  fakeModule3,
+  fakeModule4,
+  fakeModule5,
+  fakeModule6,
+} from './modules';
+
+console.log(fakeModule1, fakeModule2, fakeModule3, fakeModule4, fakeModule5, fakeModule6);
---
+++ modules.ts
@@ -0,0 +1,6 @@
+export const fakeModule1 = {}
+export const fakeModule2 = {}
+export const fakeModule3 = {}
+export const fakeModule4 = {}
+export const fakeModule5 = {}
+export const fakeModule6 = {}
